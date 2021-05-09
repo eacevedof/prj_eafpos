4feb1fcd6ac1476b01e200941d6b953a1cf87ad0
ioedumac <eacevedof@yahoo.es>
2021-05-09 18:52:54 +0200

    keyboard numbers ok

---
+++ frontend/restrict/src/components/bootstrap/app/kb-numbers/kb_numbers.js
@@ -0,0 +1,156 @@
+import { findLastIndex } from 'lodash';
+import React, { useState } from 'react';
+
+function KbNumbers({message, type}) {
+
+  const [input, set_input] = useState("")
+
+  const css = {
+    container : {
+      position: "absolute",
+      //display: "flex",
+      top:0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      margin: "auto",
+      padding: "3px",
+      width: "510px",
+      height: "505px",
+      border: "1px solid red"
+      //backgroundColor: "blue",
+    },
+
+    button : {
+      width: "160px",
+      height: "95px",
+      fontSize: "32px",
+      fontWeight: "bold",
+    },
+
+    button_del : {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "155px",
+      height: "57px",
+      fontSize: "32px",
+      fontWeight: "bold",
+    }    
+  }
+
+  const on_clear = () => {
+    set_input(input.slice(0,-1))
+  }
+
+  const on_nok = () => {
+    set_input("")
+  }
+
+  const on_ok = () => {
+    console.log("on_ok",input)
+  }
+
+  const on_click = (n) => {
+    console.log(n)
+    set_input(input.concat(n))
+  }
+
+  const updateform = ()=> {
+    console.log(input)
+  }
+
+  return (
+    <div className="container" style={css.container}>
+      
+      <div className="row">
+        <div className="col-sm p-1 input-group input-group-lg">
+          <input type="password" className="form-control" 
+            value={input}
+            onChange={updateform}
+            readOnly="readonly"
+          />
+        </div>
+        <div className="col-sm p-1">
+          <button className="btn btn-primary" type="button" style={css.button_del}
+            onClick={on_clear}
+          >&lt;</button>
+        </div>
+      </div>        
+      
+      <div className="row pb-1">
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click(7)}
+          >7</button>
+        </div>
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click(8)}
+          >8</button>
+        </div>
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click(9)}
+          >9</button>
+        </div>
+      </div>  
+      
+      <div className="row pb-1">
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click(4) }
+          >4</button>
+        </div>
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click(5)}
+          >5</button>
+        </div>
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click(6)}
+          >6</button>
+        </div>
+      </div>
+
+      <div className="row pb-1">
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click(1) }
+          >1</button>
+        </div>
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click(2)}
+          >2</button>
+        </div>
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click(3)}
+          >3</button>
+        </div>
+      </div>      
+
+      <div className="row pb-1">
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={on_nok}
+          >X</button>
+        </div>
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click(0)}
+          >0</button>
+        </div>
+        <div className="col-sm p-0">
+          <button className="btn btn-primary" type="button" style={css.button}
+            onClick={() => on_click("ok")}
+          >OK</button>
+        </div>
+      </div>     
+    </div>
+  )
+}
+
+export default KbNumbers;
--- frontend/restrict/src/modules/login/forms/login_insert.js
+++ frontend/restrict/src/modules/login/forms/login_insert.js
@@ -11,6 +11,8 @@ import SubmitAsync from 'components/bootstrap/button/submitasync'
 import Breadscrumb from 'components/bootstrap/breadscrumb/breadscrumb'
 import Footer from "components/common/footer"
 
+import KbNumbers from "components/bootstrap/app/kb-numbers/kb_numbers"
+
 function LoginInsert() {
 
   const [issubmitting, set_issubmitting] = useState(false)
@@ -124,10 +126,8 @@ function LoginInsert() {
   return (
     <>
       <Navbar />
-      <main className="container">
-        
-        <h1 className="mt-2 mb-2">login insert</h1>
-        <Breadscrumb urls={MODCONFIG.SCRUMBS.GENERIC}/>
+      <main className="container">       
+        <h1 className="mt-2 mb-2">Access code</h1>
 
         <form className="row g-3" onSubmit={on_submit}>
           {success!==""? <AlertSimple message={success} type="success" />: null}
@@ -136,94 +136,7 @@ function LoginInsert() {
           {success!==""? <ToastSimple message={success} title="Success" isvisible={true} />: null}
           {error!==""? <ToastSimple message={error} title="Error" isvisible={true} />: null}
 
-          <div className="col-md-3">
-            <label htmlFor="txt-code_erp" className="form-label">Code</label>
-            <input type="text" className="form-control" id="txt-code_erp" placeholder="code in your system" 
-            
-              ref={refcode}
-              value={formdata.code_erp}
-              onChange={updateform}
-              required 
-            />
-          </div>
-
-          <div className="col-12">
-            <label htmlFor="txt-description" className="form-label">Description</label>
-            <input type="text" className="form-control" id="txt-description" placeholder="Name of login" 
-            
-            value={formdata.description}
-            onChange={updateform}
-            required 
-            />
-          </div>
-          
-          <div className="col-12">
-            <label htmlFor="txt-description_full" className="form-label">Description large</label>
-            <textarea className="form-control" id="txt-description_full" rows="2" placeholder="large description use # if needed upto 3000 chars"
-              value={formdata.description_full}
-              onChange={updateform}
-              required 
-            ></textarea>
-          </div> 
-
-          <div className="col-md-4">
-            <label htmlFor="num-price_sale" className="form-label">Price</label>
-            <input type="number" className="form-control" id="num-price_sale" placeholder="price in default currency" 
-              value={formdata.price_sale}
-              onChange={updateform}
-              required    
-            />
-          </div>
-
-          <div className="col-md-4">
-            <label htmlFor="num-price_sale1" className="form-label">Price 1</label>
-            <input type="number" className="form-control" id="num-price_sale1" placeholder="price in second currency" 
-              value={formdata.price_sale1}
-              onChange={updateform}
-              required
-            />
-          </div>
-
-          <div className="col-md-4">
-            <label htmlFor="num-order_by" className="form-label">Order</label>
-            <input type="number" className="form-control" id="num-order_by" 
-              value={formdata.order_by}
-              onChange={updateform}
-              required            
-            />
-          </div>          
-
-          <div className="col-md-6">
-            <label htmlFor="sel-display" className="form-label">Display</label>
-            <select id="sel-display" className="form-select"
-              value={formdata.display}
-              onChange={updateform}
-              required
-            >
-              <option defaultValue>Choose...</option>
-              {
-                seldisplay.map(obj => (<option key={obj.value} value={obj.value}>{obj.text}</option>))
-              }
-            </select>
-          </div>
-
-          <div className="col-md-6">
-            <div className="form-group">
-              <label htmlFor="file-url_image" className="form-label">Picture: </label>
-              <input type="file" className="form-control" id="file-url_image" 
-                onChange={updateform}
-              />
-              <small>max: {maxsize}</small>
-              {
-                !is_empty(formdata.url_image) && is_defined(formdata.url_image.name) ?
-                (<ul>
-                  <li><small>filename: {formdata.url_image.name}</small></li>
-                  <li><small>size: {formdata.url_image.size}</small></li>
-                </ul>)
-                :null
-              }
-            </div>
-          </div>
+          <KbNumbers/>
 
           <div className="col-12">
             <SubmitAsync innertext="Save" type="primary" issubmitting={issubmitting} />
