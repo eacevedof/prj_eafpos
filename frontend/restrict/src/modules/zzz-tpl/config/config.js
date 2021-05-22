437509d2220821eff109c16db727a8672344dd73
ioedumac <eacevedof@yahoo.es>
2021-05-22 21:49:49 +0200

    corrigiendo tags

--- frontend/restrict/src/modules/zzz-tpl/config/config.js
+++ frontend/restrict/src/modules/zzz-tpl/config/config.js
@@ -2,8 +2,8 @@ export const MODCONFIG = {
   SCRUMBS:{
     GENERIC:[
       {url:"/admin",text:"Dashboard"},
-      {url:"/admin/zzz-tpls/1",text:"ZzzTpls"},
-      {url:"/admin/zzz-tpl/insert",text:"New table"},
+      {url:"/admin/zzz-tpls/1",text:"Tpls"},
+      {url:"/admin/zzz-tpl/insert",text:"New tpl"},
     ],
   }
 }
