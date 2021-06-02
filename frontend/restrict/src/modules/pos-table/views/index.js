09e742fd8a97cca6bed537a1c2d3cfdf5866849c
ioedumac <eacevedof@yahoo.es>
2021-06-02 20:44:51 +0200

    modal example

--- frontend/restrict/src/modules/pos-table/views/index.js
+++ frontend/restrict/src/modules/pos-table/views/index.js
@@ -34,6 +34,17 @@ function TableIndex() {
     set_issubmitting(false)
   }
 
+  const modal = () => {
+    const myModal = document.getElementById("my-modal")
+    if(myModal){
+      const m = new window.bootstrap.Modal(myModal, {
+        keyboard: false
+      })
+      m.show()
+    }
+
+  }
+
   useEffect(() => {
     async_onload()
     return () => console.log("login.insert.unmounting")
@@ -56,7 +67,7 @@ function TableIndex() {
           tmp.shift()
           elements.push(
             <div key={get_uuid()}>
-              <button type="button" className={`btn pos-btn-table ${otable.btn_state}`}>
+              <button type="button" className={`btn pos-btn-table ${otable.btn_state}`} onClick={modal}>
               {otable.code_erp}
               </button>
               <ul className="ul-data">
@@ -94,6 +105,23 @@ function TableIndex() {
         {render()}
         </div>
         <Footer />
+        <div id="my-modal" className="modal" tabIndex="-1">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Modal title</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <p>Modal body text goes here.</p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
   )
 }
