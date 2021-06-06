d2d057424c08a952a5c27c366286f4c7fc86bb21
ioedumac <eacevedof@yahoo.es>
2021-06-06 16:16:41 +0200

    product index

--- frontend/restrict/src/modules/adm-app-product/views/index.js
+++ frontend/restrict/src/modules/adm-app-product/views/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useMemo, useCallback} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useParams, useHistory} from "react-router-dom"
 import {MODCONFIG} from "modules/adm-app-product/config/config"
 
