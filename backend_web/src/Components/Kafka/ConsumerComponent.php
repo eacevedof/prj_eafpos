cd8b4ca7cdde7d6cc95567eda980eefd33f20038
ioedumac <eacevedof@yahoo.es>
2021-06-25 20:08:53 +0200

    kafka factory

---
+++ backend_web/src/Components/Kafka/ConsumerComponent.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link eduardoaf.com
+ * @name App\Components\Kafka\ConsumerComponent
+ * @file KafkaConsumerComponent.php 1.0.0
+ * @date 21-06-2020 21:04 SPAIN
+ * @observations
+ */
+namespace App\Components\Kafka;
+
+use App\Traits\LogTrait;
+use \RdKafka\Conf;
+use \RdKafka\Consumer;
+use \RdKafka\TopicConf;
+use \RdKafka\ConsumerTopic;
+
+
+final class ConsumerComponent
+{
+    use LogTrait;
+
+    private const KAFKA_TOPIC = "queue-logs";
+    private const REQUEST_SLEEP_TIME = 12 * 1000;
+    private const KAFKA_SOCKET = "prj_eafpos_kafka_1:9092";
+
+    private static $consumer;
+
+    private function get_config(): Conf
+    {
+        $conf = new Conf();
+        $conf->set("bootstrap.servers", self::KAFKA_SOCKET);
+        $conf->set("group.id", "test-consumer-group");
+        return $conf;        
+    }
+    
+    private function get_topic(): TopicConf
+    {
+        $topicconf = new TopicConf();
+        $topicconf->set("request.required.acks", 1);
+        $topicconf->set("auto.commit.enable", 0);
+        $topicconf->set("auto.commit.interval.ms", 100);
+        $topicconf->set("offset.store.method", "broker");
+        return $topicconf;
+    }
+    
+    private function get_consumer(): Consumer
+    {
+        if(!self::$consumer) {
+            $config = $this->get_config();
+            $consumer = new Consumer($config);
+            self::$consumer = $consumer;
+        }
+        return self::$consumer;
+    }
+    
+    private function get_consumer_topic(): ConsumerTopic
+    {
+        $consumer = $this->get_consumer();
+        $topicconf = $this->get_topic();
+        $topic = $consumer->newTopic(self::KAFKA_TOPIC, $topicconf);
+        $topic->consumeStart(0, RD_KAFKA_OFFSET_END);
+        return $topic;
+    }
+
+    public function run($fn_onresponse): void
+    {
+        $topic = $this->get_consumer_topic();
+
+        $i = 0;
+        while (true) {
+            $message = $topic->consume(0, self::REQUEST_SLEEP_TIME);
+            $now = date("Y-m-d H:i:s");
+
+            if (is_null($message)) {
+                $message = "No more messages: $now";
+                $this->logkafka($message,"kafkalogs 1");
+                continue;
+            }
+
+            switch ($message->err) {
+                case RD_KAFKA_RESP_ERR_NO_ERROR:
+                    echo "RD_KAFKA_RESP_ERR_NO_ERROR\n";
+                    if(is_callable($fn_onresponse)) call_user_func_array($fn_onresponse, [$message->payload]);
+
+                    $this->logkafka($message->payload,"kafkalogs 2 saving in db ($i) $now");
+                    break;
+                case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+                    $this->logkafka("No more messages; will wait for more","kafkalogs 2 saving in db ($i) $now");
+                    break;
+                case RD_KAFKA_RESP_ERR__TIMED_OUT:
+                    $this->logerr("timeout","kafkalogs 3 ($i) $now");
+                    break;
+                default:
+                    $this->logerr($message->errstr(),"kafkalogs 4 ($i) $now");
+                    throw new \Exception($message->errstr(), $message->err);
+                    break;
+            }
+            $i++;
+        }
+    }//run    
+}
--- backend_web/src/Services/Kafka/LogConsumerService.php
+++ backend_web/src/Services/Kafka/LogConsumerService.php
@@ -9,77 +9,26 @@
  */
 namespace App\Services\Kafka;
 
+use App\Factories\KafkaFactory;
 use App\Traits\LogTrait;
-use \RdKafka\Conf;
-use \RdKafka\Consumer;
-use \RdKafka\TopicConf;
-use \RdKafka\ConsumerTopic;
 
 final class LogConsumerService
 {
     use LogTrait;
-
-    private const KAFKA_TOPIC = "queue-logs";
-    private const REQUEST_SLEEP_TIME = 12 * 1000;
-    private const KAFKA_SOCKET = "prj_eafpos_kafka_1:9092";
-
-    private function get_consumer_topic(): ConsumerTopic
-    {
-        $conf = new Conf();
-        $conf->set("bootstrap.servers", self::KAFKA_SOCKET);
-        $conf->set("group.id", "test-consumer-group");
-
-        $consumer = new Consumer($conf);
-
-        $topicConf = new TopicConf();
-        $topicConf->set("request.required.acks", 1);
-        $topicConf->set("auto.commit.enable", 0);
-        $topicConf->set("auto.commit.interval.ms", 100);
-        $topicConf->set("offset.store.method", "broker");
-
-        $topic = $consumer->newTopic(self::KAFKA_TOPIC, $topicConf);
-        $topic->consumeStart(0, RD_KAFKA_OFFSET_END);
-        return $topic;
-    }
 
     private function save_into_db()
     {
 
     }
 
+    public function __invoke($payload): void
+    {
+        echo "====consumer service===";
+        print_r($payload);
+    }
+
     public function run(): void
     {
-        $topic = $this->get_consumer_topic();
-
-        $i = 0;
-        while (true) {
-            $message = $topic->consume(0, self::REQUEST_SLEEP_TIME);
-            $now = date("Y-m-d H:i:s");
-
-            if (is_null($message)) {
-                $message = "No more messages: $now";
-                $this->logkafka($message,"kafkalogs 1");
-                continue;
-            }
-
-            switch ($message->err) {
-                case RD_KAFKA_RESP_ERR_NO_ERROR:
-                    echo "RD_KAFKA_RESP_ERR_NO_ERROR\n";
-                    $this->logkafka($message->payload,"kafkalogs 2 saving in db ($i) $now");
-                break;
-                case RD_KAFKA_RESP_ERR__PARTITION_EOF:
-                    $this->logkafka("No more messages; will wait for more","kafkalogs 2 saving in db ($i) $now");
-                break;
-                case RD_KAFKA_RESP_ERR__TIMED_OUT:
-                    $this->logerr("timeout","kafkalogs 3 ($i) $now");
-                break;
-                default:
-                    $this->logerr($message->errstr(),"kafkalogs 4 ($i) $now");
-                    throw new \Exception($message->errstr(), $message->err);
-                break;
-            }
-            $i++;
-        }
-    }//run
-
+        KafkaFactory::get_consumer()->run($this);
+    }
 }
