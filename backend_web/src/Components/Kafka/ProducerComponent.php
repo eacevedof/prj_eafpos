1f5d2c313c15d7cfd50ace9bd80d57d5bc7c7ecb
ioedumac <eacevedof@yahoo.es>
2021-06-25 23:07:45 +0200

    fix logs producer

--- backend_web/src/Components/Kafka/ProducerComponent.php
+++ backend_web/src/Components/Kafka/ProducerComponent.php
@@ -34,14 +34,14 @@ final class ProducerComponent
             //https://github.com/eacevedof/prj_docker_imgs/blob/master/kafka/php/kafka/producer-2.php
             $CONFIG["callbacks"]["on_success"] = function ($kafka, $message) use ($pathkafka) {
                 file_put_contents(
-                    "$pathkafka/on_success.log",
+                    "$pathkafka/producer_success.log",
                     var_export($message, true), FILE_APPEND
                 );
             };
 
             $CONFIG["callbacks"]["on_error"] = function ($kafka, $err, $reason) use($patherror) {
                 file_put_contents(
-                    "$patherror/on_error.log",
+                    "$patherror/producer_error.log",
                     sprintf("Kafka error: %s (reason: %s)", rd_kafka_err2str($err), $reason) . PHP_EOL,
                     FILE_APPEND
                 );
