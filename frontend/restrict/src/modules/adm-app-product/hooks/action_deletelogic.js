807ace5833b24095b7835c2661ae9ccddf05a1dc
ioedumac <eacevedof@yahoo.es>
2021-07-10 17:50:41 +0200

    delet-logic.js

--- frontend/restrict/src/modules/adm-app-product/hooks/action_deletelogic.js
+++ frontend/restrict/src/modules/adm-app-product/hooks/action_deletelogic.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {useParams} from "react-router-dom"
 import {MODCONFIG} from "modules/adm-app-product/config/config"
 import {async_get_by_id, async_deletelogic} from "modules/adm-app-product/async/async_repository"
@@ -26,26 +26,14 @@ const formdefault = {
   url_image: "",
 }
 
-const sysfields = {
-  insert_user:"",
-  insert_date:"",
-  update_date:"",
-  update_user:"",
-  delete_user:"",
-  delete_date:"",
-}
-
 function ActionDeleteLogic(){
 
   const {id} = useParams()
-  const [issubmitting, set_issubmitting] = useState(false)
   const [error, set_error] = useState("")
   const [success, set_success] = useState("")
+  const [issubmitting, set_issubmitting] = useState(false)
   const [isdeleted, set_isdeleted] = useState(false)
-
   const [formdata, set_formdata] = useState(formdefault)
-
-  const [sysdata, set_sysdata] = useState(sysfields)
 
   const before_submit = () => {}
 
@@ -54,9 +42,7 @@ function ActionDeleteLogic(){
   }
 
   const on_submit = async evt => {
-    console.log("product.deletelogic.on_submit.formdata:",formdata)
     evt.preventDefault()
-
     set_issubmitting(true)
     set_error("")
     set_success("")
@@ -65,7 +51,6 @@ function ActionDeleteLogic(){
 
     try {
       const r = await async_deletelogic(formdata)
-      console.log("product.deletelogic.on_submit.r",r)
       set_success("Num regs deleted: ".concat(r))
       await async_onload()
     }
@@ -77,18 +62,14 @@ function ActionDeleteLogic(){
     }
   }
 
-  const async_onload = async () => {
+  const async_onload = useCallback(async () => {
     set_issubmitting(true)
 
     try {
       const r = await async_get_by_id(id)
-      console.log("product.deletelogic.onload.r",r)
       const tmpform = {...formdata, ...r}
-      console.log("product.deletelogic.onload.tmpform",tmpform)
       set_formdata(tmpform)
       if(r.delete_date!=="" && r.delete_date!==null) set_isdeleted(true)
-      set_sysdata({...tmpform})
-      console.log("product.deletelogic.onload.formdata:",formdata)
     }
     catch(error){
       set_error(error)
@@ -96,7 +77,7 @@ function ActionDeleteLogic(){
     finally{
       set_issubmitting(false)
     }
-  }
+  },[])
 
   useEffect(()=>{
     async_onload()
@@ -108,13 +89,12 @@ function ActionDeleteLogic(){
     success,
     error,
     formdata,
-    sysdata,
-
-    on_submit,
     issubmitting,
     isdeleted,
-    async_refresh,
     seldisplay,
+
+    async_refresh,
+    on_submit,
   }
 }
 
