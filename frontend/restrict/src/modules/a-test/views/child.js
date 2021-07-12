91bbd0b62e29fc04a1e5fae6256acf9c2c99b8bb
ioedumac <eacevedof@yahoo.es>
2021-07-12 22:44:08 +0200

    test module ok

--- frontend/restrict/src/modules/a-test/views/child.js
+++ frontend/restrict/src/modules/a-test/views/child.js
@@ -1,11 +1,13 @@
 import React, {useEffect} from "react"
 
+//el componente se renderiza nuevamente si le llegan las variables de las propiedades
 function Child({msg1, msg2}){
 
   useEffect(()=>{
-    console.log("child mounting")
-    return ()=> console.log("child unmounting")
-  },[])
+    //esta funcion solo se dispara si hay un cambio en la variable msg1 pq la esta escuchando
+    console.log("child mounting", msg1)
+    return ()=> console.log("child unmounting por:", msg1)
+  },[msg1])
 
   return (
     <>
--- frontend/restrict/src/modules/a-test/views/index.js
+++ frontend/restrict/src/modules/a-test/views/index.js
@@ -1,8 +1,10 @@
-import React, {useEffect} from "react"
+import React, {useEffect, useState} from "react"
 import {NavLink} from "react-router-dom"
 import Child from "./child"
 
 function TestIndex() {
+
+  const [msgd, set_msgd] = useState("")
 
   useEffect(()=>{
     console.log("parent mounting")
@@ -12,8 +14,9 @@ function TestIndex() {
   return (
     <>
       <NavLink className="btn btn-primary" activeClassName="navlink-active" exact to={"/admin"}>Admin</NavLink>
+      <button onClick={() => set_msgd("ab-c")}>change</button>
       <h1>Test Index</h1>
-      <Child msg1={"hola"} msg2={"mundo"} />
+      <Child msg1={msgd} msg2={"mundo"} />
       <Child msg1={"hola 222"} msg2={"mundo 222"} />
     </>
   )
