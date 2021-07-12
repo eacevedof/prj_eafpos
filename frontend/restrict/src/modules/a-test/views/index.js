034f3e2631964454fee98bf46ba383bbac893d3d
ioedumac <eacevedof@yahoo.es>
2021-07-12 22:33:12 +0200

    test module ok

--- frontend/restrict/src/modules/a-test/views/index.js
+++ frontend/restrict/src/modules/a-test/views/index.js
@@ -14,6 +14,7 @@ function TestIndex() {
       <NavLink className="btn btn-primary" activeClassName="navlink-active" exact to={"/admin"}>Admin</NavLink>
       <h1>Test Index</h1>
       <Child msg1={"hola"} msg2={"mundo"} />
+      <Child msg1={"hola 222"} msg2={"mundo 222"} />
     </>
   )
 }
