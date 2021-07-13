9f0225d9f5679fd2b0da6787ea9926109b776f11
ioedumac <eacevedof@yahoo.es>
2021-07-13 19:28:01 +0200

    se pinta 2 veces cada uno pq?

--- frontend/restrict/src/modules/a-test/views/child.js
+++ frontend/restrict/src/modules/a-test/views/child.js
@@ -2,9 +2,10 @@ import React, {useEffect, useState} from "react"
 
 //el componente se renderiza nuevamente si le llegan las variables de las propiedades
 function Child({msg1, msg2}){
-
+  console.log("function Child")
   const [ch, set_ch] = useState(":)")
 
+  /*
   useEffect(()=>{
     //esta funcion solo se dispara si hay un cambio en la variable msg1 pq la esta escuchando.
     //si hay un cambio en msg1 primero se ejecut el unmount y despues el mount
@@ -13,6 +14,7 @@ function Child({msg1, msg2}){
     return ()=> console.log("child unmounting por:", msg1, msg2)
   },[msg1])
 
+   */
   return (
     <>
       <div className="container">
--- frontend/restrict/src/modules/a-test/views/index.js
+++ frontend/restrict/src/modules/a-test/views/index.js
@@ -1,24 +1,26 @@
-import React, {useEffect, useState} from "react"
+import React, {memo, useEffect, useMemo, useState} from "react"
 import {NavLink} from "react-router-dom"
 import Child from "./child"
 
 function TestIndex() {
-
+  console.log("function Parent")
   const [msgd, set_msgd] = useState("-")
 
+/*
   useEffect(()=>{
     console.log("parent mounting")
     return ()=> console.log("parent unmounting")
   },[])
+*/
 
   return (
     <>
       <NavLink className="btn btn-primary" activeClassName="navlink-active" exact to={"/admin"}>Admin</NavLink>
-      <button onClick={() => set_msgd("ab-c")}>change</button>
+      <button onClick={() => set_msgd("ab-c")}>Set msgd en parent</button>
       <h1>Test Index</h1>
       <Child msg1={msgd} msg2={"mundo"} />
     </>
   )
 }
 
-export default TestIndex;
+export default memo(TestIndex);
