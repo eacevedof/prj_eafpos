65842e3343f94d009d55b58dc86cdf602b10f3b4
ioedumac <eacevedof@yahoo.es>
2021-09-12 16:05:01 +0200

    front modal como wrapper

--- frontend/restrict/src/modules/a-test/views/parent.js
+++ frontend/restrict/src/modules/a-test/views/parent.js
@@ -5,23 +5,34 @@ import "components/bootstrap/modal/modalraw.css"
 
 function Parent() {
 
-	const [is_opened, open_modal, close_modal] = useModal()
+	const [is_opened_lm, open_modal_lm, close_modal_lm] = useModal()
+	const [is_opened_cm, open_modal_cm, close_modal_cm] = useModal()
 
   return (
     <div className="container">
       <br/>
       <h1>Test Index</h1>
-			<button onClick={open_modal}>open modal</button>
+			<button onClick={open_modal_lm}>open login modal</button>
+			<button onClick={open_modal_cm}>open chat modal</button>
+
 			<ModalRaw
-				title={"hola mundo"}
-				is_open={is_opened}
-				fn_close={close_modal}
+				title={"login"}
+				is_open={is_opened_lm}
+				fn_close={close_modal_lm}
 			>
-				<h1>Login</h1>
 				<form>
 					<input type="email" placeholder="correo"/>
 					<input type="password" placeholder="contraseña"/>
 				</form>
+			</ModalRaw>
+
+			<ModalRaw
+				title={"chat"}
+				is_open={is_opened_cm}
+				fn_close={close_modal_cm}
+			>
+				<p>Hola!! <b>Eaf</b></p>
+				<p>Que hace :v <b>Maria</b></p>
 			</ModalRaw>
     </div>
   )
